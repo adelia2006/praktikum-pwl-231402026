fb5ff99736b5b7f4810f46fc470a40b78771e1bd
adelia2006 <151592045+adelia2006@users.noreply.github.com>
2024-04-25 13:59:21 +0700

    tugas 3

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,26 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\TodoTaskController;
 
-Route::get('/', function () {
-    return view('home');
-});
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+// Route::get('/', function () {
+// //     return view('home');
+// });
+// $tasks = [
+//     [
+//         'task'    => 'Task1',
+//         'tanggal' => '2022-03-21',
+//     ],
+//     [
+//         'task'    => 'Task2',
+//         'tanggal' => '2022-03-22',
+//     ]
+// ];
+Route::get('/', [TodoTaskController::class, 'index']);
+Route::post('/', [TodoTaskController::class, 'store'])->name('tasks.store');
+Route::delete('/deleteTask/{id}', [TodoTaskController::class, 'deleteTask']);
